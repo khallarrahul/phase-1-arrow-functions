2089dcc1097e2c3abe8c117a74e9059eff0068d7
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-03-17 14:51:20 -0700

    adds arrow

---
+++ index.js
@@ -0,0 +1,15 @@
+// write your code here
+
+const divide = (num1, num2) => {
+    return num1/num2;
+}
+
+
+const square = (num1, num2) => {
+    return num1**2
+}
+
+
+const add = (num3,num4) =>{
+    return num3 + num4
+}
